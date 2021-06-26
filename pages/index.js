e1e76c4cf49ff995df026eca6042f7629ab6dc40
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-26 10:07:51 +0300

    Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-
+import Layout from '../components/Layout'
 
 export default function HomePage() {
   return (
-    <div >
+    <Layout>
       <h1>Hello world</h1>
-    </div>
+    </Layout>
   )
 }
