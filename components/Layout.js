6dc3786e05c718cdace41357f82e85ed079618ed
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-26 10:28:29 +0300

    Header & Tailwind Styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,26 @@
 import Head from 'next/head'
+import Header from './Header'
+
 
 export default function Layout({ title, keywords, description, children }) {
     return (
         <div>
             <Head>
-                <title> {title} </title>
+                <title>{title}</title>
                 <meta name='keywords' content={keywords} />
                 <meta name='description' content={description} />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel='icon' href='/favicon.ico' />
             </Head>
-            <main className='container mx-auto my-7'>
-                {children}
-            </main>
+
+            <Header />
+
+            <main className='container mx-auto my-7'>{children}</main>
         </div>
     )
 }
 
 Layout.defaultProps = {
     title: 'Welcome to DevSpace',
-    keywords: 'development,coding,programming',
-    description: 'The best info and news'
+    keywords: 'development, coding, programming',
+    description: 'The best info and news in development',
 }
