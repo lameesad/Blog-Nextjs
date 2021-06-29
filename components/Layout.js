aa6833847d8edfcd83684cb76017baa12db8e1ae
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-29 11:28:11 +0300

    Search Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from './Header'
+import Search from './Search'
 
 
 export default function Layout({ title, keywords, description, children }) {
@@ -13,6 +14,7 @@ export default function Layout({ title, keywords, description, children }) {
             </Head>
 
             <Header />
+            <Search />
 
             <main className='container mx-auto my-7'>{children}</main>
         </div>
